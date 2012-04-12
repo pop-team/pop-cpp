d668117060f0e3f7478470646e388158f46a8e11
Valentin Clement <clementval@gmail.com>
2012-04-12 12:23:15 +0000

    - Fix service name

--- services/popfilemanager_obj.cc
+++ services/popfilemanager_obj.cc
@@ -1,3 +1,12 @@
+/**
+ * File : popfilemanager_obj.cc
+ * Author : Valentin Clement
+ * Description : Transition file to produce executable service
+ * Creation date : 2012/04/06
+ * 
+ * Modifications : 
+ * Authors		Date			Comment
+ * clementval		2012/04/06  		Creation of this file
+ */
 #include "popfilemanager.ph"
-
-@pack(POPFileManger)
+@pack(POPFileManager)
