7ba4347e0672cf8837aff900ab04d914c0cae12e
Valentin Clement <clementval@gmail.com>
2012-05-07 15:00:36 +0000

    - Add reading test

--- dev/popfile/main2.cpp
+++ dev/popfile/main2.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 
    long mtime, seconds, useconds;  
 	
-	cout << "[POPFILE] Start of POPFile prototype test program:" << endl;
+//	cout << "[POPFILE] Start of POPFile prototype test program:" << endl;
 
 
    gettimeofday(&start, NULL);	
@@ -30,17 +30,38 @@ int main(int argc, char** argv)
 		pfstream << "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
 		pfstream << "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
 		pfstream << "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
-		pfstream << "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";																			pfstream << "stop_1024_//";	
+		pfstream << "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";															pfstream << "stop_1024_//";	
 	}		
 	
-	pfstream.close();
 
+	pfstream.close();
    gettimeofday(&end, NULL);
+   
+   int length=0;
+   
+	ifstream strip;
+	strip.open("/tmp/.hugefile_strip0", ios::binary);
+	
+	strip.seekg (0, ios::end);		// Seek to end
+	length = strip.tellg();			// Get length of the strip
+	
+	strip.seekg (0, ios::beg);
+
+
+	char* buffer;
+	long offset = 10000000;
+	buffer = new char[offset];
+	strip.read(buffer, offset-1);
+	
+	strip.close();
+	
+	cout << buffer << endl;
+
 
    seconds  = end.tv_sec  - start.tv_sec;
    useconds = end.tv_usec - start.tv_usec;
  
    mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
 	
-	cout << "[POPFILE] End of POPFile prototype test program: [ms]" << mtime << endl;
+//	cout << "[POPFILE] End of POPFile prototype test program: [ms]" << mtime << endl;
 }
