191452a02b8b4b1b72003cbfd2995e8a04483032
Valentin Clement <clementval@gmail.com>
2012-05-21 08:15:21 +0000

    - Add test case: write to already existing file

--- dev/popfile/main.cpp
+++ dev/popfile/main.cpp
@@ -1,4 +1,5 @@
 #include "popfile.h"
+#include "popfile_grip.h"
 #include <sys/time.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -15,8 +16,9 @@ int main(int argc, char** argv)
 	if(argc < 2){
 		cout << "[POPFILETEST] Please provide a test case identifier:" << popcendl;
 		cout << "[POPFILETEST] 1. Test creation and writing process" << popcendl;
-		cout << "[POPFILETEST] 2. Test opening and reading process" << popcendl;
-		cout << "[POPFILETEST] 3. Test opening of a standard file and scatter" << popcendl;	
+		cout << "[POPFILETEST] 2. Test opening and write to end of file" << popcendl;		
+		cout << "[POPFILETEST] 3. Test opening and reading process" << popcendl;
+		cout << "[POPFILETEST] 4. Test opening of a standard file and scatter" << popcendl;	
 		cout << "[POPFILETEST] Test case number:" << popcendl;	
 		std::cin >> testcase;	
 		cout << "[POPFILETEST] Running test case: " << testcase << popcendl;	
@@ -27,6 +29,7 @@ int main(int argc, char** argv)
 
    long mtime, seconds, useconds;  
 	POPFStream pfstream;
+	
    switch(testcase){
    	case 1: {
 	   	gettimeofday(&start, NULL);	
@@ -42,7 +45,7 @@ int main(int argc, char** argv)
 				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
 				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
 				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
-				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");													pfstream.write("stop_1024___");		
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");												pfstream.write("stop_1024___");		
 			}		
 			pfstream.close();			
 			gettimeofday(&end, NULL);
@@ -54,12 +57,34 @@ int main(int argc, char** argv)
    		break;
    	case 2: {
    		gettimeofday(&start, NULL);	
+			pfstream.open(FILE2);
+			for (int i = 0; i < 100; i++){
+				pfstream.write("start1024___");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+				pfstream.write("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");												pfstream.write("stop_1024___");		
+			}		
+			pfstream.close();			
+			gettimeofday(&end, NULL);
+			seconds  = end.tv_sec  - start.tv_sec;
+			useconds = end.tv_usec - start.tv_usec;
+			mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;			
+   		cout << "[POPFILETEST] Writing time : [ms]" << mtime << popcendl;
+   		}
+   		break; 		
+   	case 3: {
+   		gettimeofday(&start, NULL);	
 			pfstream.open(FILE2, 4, 10000000);
-			pfstream.read_in_background();
+			POPFileGrip grip = pfstream.read_in_background(25000000);
 			sleep(4);
-			std::string data = pfstream.get_read();
-			cout << data.substr(2048, 2048) << popcendl;
-			data = pfstream.get_read();
+			std::string data = pfstream.get_read(grip);
 			cout << data.substr(2048, 2048) << popcendl;
 			gettimeofday(&end, NULL);
 			pfstream.close();
@@ -69,7 +94,7 @@ int main(int argc, char** argv)
    		cout << "[POPFILETEST] Reading time : [ms]" << mtime << popcendl;			
    		}
    		break;	
-   	case 3: {
+   	case 4: {
 	   		pfstream.open("testfile");
 	   		if(pfstream.is_open()) {
 	   			cout << "[POPFILETEST] File is open" << popcendl;
