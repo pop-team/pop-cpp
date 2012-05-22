cf4e79ccc7f61750ba071c56381cd87ab14724a6
Valentin Clement <clementval@gmail.com>
2012-05-22 10:31:54 +0000

    - Push after automake and autoreconf

--- config.h
+++ config.h
@@ -11,7 +11,7 @@
 #define HAVE_LIBDL 1
 
 /* Define to 1 if you have the `nsl' library (-lnsl). */
-#define HAVE_LIBNSL 1
+/* #undef HAVE_LIBNSL */
 
 /* Define to 1 if you have the `pthread' library (-lpthread). */
 #define HAVE_LIBPTHREAD 1
