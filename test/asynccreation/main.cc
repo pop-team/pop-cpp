952e6c1a95b153fac42377003d722814d6f96f54
Valentin Clement <clementval@gmail.com>
2012-08-22 11:45:50 +0000

    - Add more parallel object to create with async allocation

--- test/asynccreation/main.cc
+++ test/asynccreation/main.cc
@@ -2,8 +2,8 @@
 
 /** 
  * @author  clementval
- * @date    2012.07.11
- * This program tests compilation without the @pack directive. Compilation should work and print a warning message.
+ * @date    2012.08.22
+ * This program tests asynchronous parallel object allocation
  */
 int main(int argc, char** argv)
 {
@@ -15,6 +15,13 @@ int main(int argc, char** argv)
 		POPObject o2;
 		printf("Before creating POPObject o3\n");		
 		POPObject o3;
+		POPObject o4;
+		POPObject o5;
+		POPObject o6;
+		POPObject o7;
+		POPObject o8;
+		POPObject o9;
+		POPObject o10;										
 		printf("Before calling method 1 on POPObject o1\n");		
 		o1.firstMethod();
 		printf("Before calling method 1 on POPObject o2\n");		
