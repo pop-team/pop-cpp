475fb92986a40d89a99ee1121bab4fcc371b98b5
Valentin Clement <clementval@gmail.com>
2012-09-25 00:51:45 +0000

    - Fix serialization of enum

--- examples/serialize_enum/data.cc
+++ examples/serialize_enum/data.cc
@@ -27,14 +27,14 @@ void Data::Serialize(POPBuffer &buf, bool pack)
 	if (pack){
 		buf.Pack(&theData, 1);
 		// Serialize the enum
-		//int i = (int)te;
-		buf.Pack(&te, 1);
+		int i = (int)te;
+		buf.Pack(&i, 1);
 	} else {
 		buf.UnPack(&theData, 1);
 		// Unserialize the enum
-		//int i; 
-		buf.UnPack(&te, 1);
-//		te = (TestEnum)i;
+		int i; 
+		buf.UnPack(&i, 1);
+		te = (TestEnum)i;
 	}
 }
 
