1aa49b720d8abf47b413f9809f032907ceac720f
Valentin Clement <clementval@gmail.com>
2012-10-26 07:41:50 +0000

    - Little modification for K Computer version

--- test/callback/toto.cc
+++ test/callback/toto.cc
@@ -4,7 +4,7 @@
 
 Toto::Toto()
 {
-  printf("CallBack: Object Toto on \"%s\"\n", POPGetHost());
+  printf("CallBack: Object Toto on \"%s\"\n", GetAccessPoint().GetAccessString());
   ident = 0;
 }
 
@@ -25,7 +25,10 @@ int Toto::GetIdent()
 
    Titi t;                 // create an object to call me back
    this->SetIdent(222);    // test of "this"
-   t.ComputeIdent(*this);  // t is going to call me back
+   Toto* tmp = new Toto(GetAccessPoint());
+   t.ComputeIdent((*tmp));  // t is going to call me back   
+   //t.ComputeIdent(*this);  // t is going to call me back
+   delete tmp;
    return(ident);          // ident has changed because of the call back
 }
 
--- test/callback/toto.ph
+++ test/callback/toto.ph
@@ -7,7 +7,7 @@ parclass Toto
 classuid(1002);
 
 public:
-	Toto() @{od.url("localhost");};
+	Toto() @{ od.url("localhost"); od.executable("/Users/clementval/versioning/popc/popc2.6.2/test/callback/toto.obj"); };
 	~Toto();
 
 	seq sync void SetIdent(int i);
