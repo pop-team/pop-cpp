ff818d249461f2bb495a2065751135bc93998d24
Valentin Clement <clementval@gmail.com>
2012-10-30 06:49:07 +0000

    - Update test for 2.6.2

--- examples/integer/main.cc
+++ examples/integer/main.cc
@@ -1,8 +1,6 @@
-
-
-
 #include "integer.ph"
 #include <iostream>
+#include <assert.h>
 
 using namespace std;
 
@@ -12,18 +10,16 @@ int main(int argc, char* argv[])
 	try {
 		// Create 2 Integer objects
 		Integer o1;
-
-
-
-
 		o1.Set(10);
 		printf("o1=%d\n", o1.Get());
 		Integer o2;
 		o2.Set(11);
 		printf("o2=%d\n", o2.Get());
 		o1.Add(o2);
-		cout << "o1=o1+o2; o1=" << o1.Get() << endl;
+		int result = o1.Get();
+		cout << "o1=o1+o2; o1=" << result << endl;
 		printf("End of test\n");
+		assert(result == 21);
 	} catch (POPException *e) {
 		cout << "Exception occurs in application :" << endl;
 		e->Print();
