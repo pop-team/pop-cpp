cd6e3564191c1ac47d72b949ecd86a70c49ff096
Valentin Clement <clementval@gmail.com>
2012-10-30 07:01:25 +0000

    - Push simple example. Singel parallel object creation.

---
+++ examples/simple/main.cc
@@ -0,0 +1,20 @@
+
+
+
+#include "integer.ph"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	try {
+		Integer o1;
+	} catch (POPException *e) {
+		cout << "Exception occurs in application :" << endl;
+		e->Print();
+		delete e;
+		return -1;
+	}
+	return 0;
+}
