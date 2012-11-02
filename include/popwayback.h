655a98ac2bd570471829ca395b3ee616e64fbde3
Valentin Clement <clementval@gmail.com>
2012-11-02 02:19:09 +0000

    - Remove const from return statement as it has no effect.

--- include/popwayback.h
+++ include/popwayback.h
@@ -25,23 +25,23 @@ clementval  22/10/2010  Add doxygen comment
  */
 class POPWayback : public POPBase {
 public:
-   POPWayback();  //POPWayback constructor
-   ~POPWayback(); //POPWayback destructor
+  POPWayback();  //POPWayback constructor
+  ~POPWayback(); //POPWayback destructor
    
-   //Operator
+  //Operator
 	const POPWayback & operator=(const POPWayback &x);
 
-   //Specialized methods
-   paroc_string getAsString() const;      //Get the object as a formatted string value
-   void insertNode(paroc_string nodeId);  //Insert a node a the end of the way back
-   paroc_string getNextNode() const;      //Return the next node in the object
-   void deleteNextNode();                 //Delete the next node in the object
-   const bool isLastNode() const;         //Check if it's the lasr node in the way back
-   const bool isEmpty() const;            //Check if the way back is empty
+  //Specialized methods
+  paroc_string getAsString() const;      //Get the object as a formatted string value
+  void insertNode(paroc_string nodeId);  //Insert a node a the end of the way back
+  paroc_string getNextNode() const;      //Return the next node in the object
+  void deleteNextNode();                 //Delete the next node in the object
+  bool isLastNode() const;         //Check if it's the lasr node in the way back
+  bool isEmpty() const;            //Check if the way back is empty
 
 
-   //Inherited method to serialize the object
-   virtual void Serialize(POPBuffer &buf, bool pack); 
+  //Inherited method to serialize the object
+  virtual void Serialize(POPBuffer &buf, bool pack); 
 
 private: 
    std::list<paroc_string> _lst_wb;   //List containing the path to a specific node
