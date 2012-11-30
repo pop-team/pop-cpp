c49042e3edcfbc612cca0622ef062ddd99001b52
Valentin Clement <clementval@gmail.com>
2012-11-30 09:31:46 +0000

    - Recv first message

--- xmp/popc_objmain.xmp.cc
+++ xmp/popc_objmain.xmp.cc
@@ -15,27 +15,67 @@
 
 int main(int argc, char* argv[]) 
 {
+  MPI::Intercomm communicator; 
+  int rank; 
+  if(paroc_utils::checkremove(&argc, &argv, "-listlong") != NULL) {
+    printf("This kind of parallel object doesn't support this option.\n"); 
+    return 1;
+  }
+
   if(!MPI::Is_initialized()) {
     int required_support = MPI_THREAD_SERIALIZED;
     int provided_support = MPI::Init_thread(required_support); 
-
+    
+    communicator = MPI::COMM_WORLD.Get_parent(); 
+    rank = MPI::COMM_WORLD.Get_rank();  
+    
     if(provided_support < required_support) {
       printf("POP-C++ Warning: Support of multithread in MPI is lower than required. Deadlock can block your application\n"); 
     } else {
-      printf("POP-C++ XMP process started %d\n", MPI::COMM_WORLD.Get_rank());
+      printf("POP-C++ XMP process started %d\n", rank);
     }
   }  
 
-	char *objectname = paroc_utils::checkremove(&argc, &argv, "-object=");
-	if (objectname == NULL) {
-	  printf("POP-C++ Error: Object name has not been specified in the arguments!\n");
+  char *objectname = paroc_utils::checkremove(&argc, &argv, "-object=");
+  if (objectname == NULL) {
+    printf("POP-C++ Error: Object name has not been specified in the arguments!\n");
     if(!MPI::Is_finalized()) {
       MPI::Finalize();
     }
     return 1;	   
-	}
+  }
+  
+
+
+  
+  
+  
+  
+  printf("XMP parallel object rank %d %d\n", rank, communicator.Get_remote_size());     
+
+  int length; 
+  
+  communicator.Recv(&length, 1, MPI_INT, 0, 0); 
+  printf("%d recv %d\n", MPI::COMM_WORLD.Get_rank(), length);
+
+  int data2; 
+  communicator.Recv(&data2, 1, MPI_INT, 0, 0); 
+  printf("%d recv %d\n", MPI::COMM_WORLD.Get_rank(), data2); 
+
+//  char* load = new char[length]; 
+//  communicator.Recv(load, length, MPI_CHAR, 0, 1);
+//  int data; 
+//  communicator.Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, rank);
+//  printf("XMP parallel object received %d\n", data);     
+  
+
+  
+
+
 	
-	printf("Will create object of type=%s\n", objectname); 
+  printf("XMP Object: %d Will create object of type=%s\n", rank, objectname); 
+  
+  
 
 
 
