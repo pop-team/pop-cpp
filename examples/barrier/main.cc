becf13dcafa76bc33a8fbdcb950e13a4264c8208
Laurent Winkler <laurent.winkler@bluewin.ch>
2015-03-12 13:13:23 +0100

    Merged test/barrier and example/barrier. Use symlink instead

--- examples/barrier/main.cc
+++ examples/barrier/main.cc
@@ -1,9 +1,14 @@
+/**
+ * @author  P.kuonen
+ * @date    2012.09.04
+ * This program test the implementaion of a barrier using synchronizers
+ */
 #include <stdio.h>
-#include "barrier.ph"
+#include "Barrier.ph"
 #include "worker.ph"
 
 int main(int argc, char** argv) {
-    printf("Barrier: Starting ...\n");
+    printf("Barrier: Starting test...\n");
     int Nb_workers=15;
     if(argc > 1) {
         Nb_workers = atoi(argv[1]);
@@ -16,7 +21,12 @@ int main(int argc, char** argv) {
         theWorkers[i].SetNo(i);
         theWorkers[i].Work(Bar);
     }
-    printf("Barrier: Value of theWorkers[1].GetNo()=%d\n",theWorkers[1].GetNo());
-    printf("Barrier: terminated, destroying objects...\n");
-    return 0;
+    if(theWorkers[1].GetNo()==1) {
+        printf("Barrier: test succeeded, destroying objects...\n");
+        return 0;
+    } else {
+        printf("Barrier: test failed, destroying objects...\n");
+        return 1;
+    }
+
 }
--- examples/barrier/worker.ph
+++ examples/barrier/worker.ph
@@ -1,13 +1,13 @@
 #ifndef _CWORKER_PH
 #define _CWORKER_PH
-#include "barrier.ph"
+#include "Barrier.ph"
 
 parclass Cworker {
     classuid(1002);
 
 public:
-    Cworker() @{od.url("localhost");};
-    Cworker(int No) @{od.url("localhost");};
+    Cworker() @{od.url("localhost"); od.executable("./Cworker.obj");};
+    Cworker(int No) @{od.url("localhost"); od.executable("./Cworker.obj");};
 
     async seq void Work(POP_Barrier &b);
     sync seq void SetNo(int no);
