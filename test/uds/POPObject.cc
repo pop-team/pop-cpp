2ff9d8ad180451ca299671707867236d5fbf9f13
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-27 08:40:15 +0100

    Update the test case

--- test/uds/POPObject.cc
+++ test/uds/POPObject.cc
@@ -3,19 +3,19 @@
 #include <unistd.h>
 
 POPObject::POPObject() {
-    cout << "POPObject created (by JobMgr) on machine:" << (const char*)POPSystem::GetHost() << popcendl;
+    cout << "POPObject created (by JobMgr) on machine:" << POPSystem::GetHost() << popcendl;
 }
 
 POPObject::~POPObject() {
-    cout << "POPObject on machine " << (const char*)POPSystem::GetHost() <<" is being destroyed" << popcendl;
+    cout << "POPObject on machine " << POPSystem::GetHost() <<" is being destroyed" << popcendl;
 }
 
 void POPObject::firstMethod() {
-    cout << "First method is called on " << (const char*)POPSystem::GetHost() << popcendl;
+    cout << "First method is called on " << POPSystem::GetHost() << popcendl;
 }
 
 void POPObject::secondMethod() {
-    cout << "Second method is called on " << (const char*)POPSystem::GetHost() <<popcendl;
+    cout << "Second method is called on " << POPSystem::GetHost() <<popcendl;
 }
 
 @pack(POPObject);
