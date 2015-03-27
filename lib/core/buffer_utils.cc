cbd9473ec615582d2602ed2414324c4ab8a6c111
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-27 16:12:18 +0100

    Cleanup

--- lib/core/buffer_utils.cc
+++ lib/core/buffer_utils.cc
@@ -30,16 +30,4 @@ void packarray(paroc_buffer &buf, T *param, int size, int flag, paroc_memspool* 
             param=0;
         }
     }
-
 }
-
-//TODO This should probably be removed
-template void packarray<char> (paroc_buffer &buf, char *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<unsigned char> (paroc_buffer &buf, unsigned char *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<int> (paroc_buffer &buf, int  *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<long> (paroc_buffer &buf, long *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<unsigned> (paroc_buffer &buf, unsigned  *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<short> (paroc_buffer &buf, short *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<unsigned short> (paroc_buffer &buf, unsigned short *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<double> (paroc_buffer &buf, double *param, int size, int flags, paroc_memspool *tmp);
-template void packarray<float> (paroc_buffer &buf, float *param, int size, int flags, paroc_memspool *tmp);
