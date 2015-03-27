5037db64c3d40f73ff3e6f4ab3255b192afb206e
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-27 14:11:23 +0100

    Cleanup

--- include/dynamic/popc_combox_uds.h
+++ include/dynamic/popc_combox_uds.h
@@ -21,12 +21,6 @@
 #include "paroc_combox.h"
 #include "popc_connection_uds.h"
 
-/**
- * @class paroc_combox_socket
- * @brief Socket declaration of combox, used by POP-C++ runtime.
- * @author Tuan Anh Nguyen
- *
- */
 class popc_combox_uds: public paroc_combox {
 public:
     popc_combox_uds();
@@ -37,18 +31,16 @@ public:
     virtual bool need_address(){ return true; }
 
     virtual bool Connect(const char *url);
+    virtual void Close();
+
+    virtual paroc_connection *Wait();
+    virtual paroc_connection* get_connection();
 
     virtual int Send(const char *s,int len);
     virtual int Send(const char *s,int len, paroc_connection *connection);
 
     virtual int Recv(char *s,int len);
     virtual int Recv(char *s,int len, paroc_connection *connection);
-
-    virtual paroc_connection *Wait();
-
-    virtual paroc_connection* get_connection();
-
-    virtual void Close();
 
     virtual std::string GetUrl();
     virtual std::string GetProtocol();
@@ -70,16 +62,6 @@ private:
     bool _connected;
     bool _is_first_connection;
     popc_connection_uds* _connection;
-
 };
 
-
-
 #endif // POPC_COMBOX_UNIX_DOMAIN_SOCKET_H
-
-
-
-
-
-
-
